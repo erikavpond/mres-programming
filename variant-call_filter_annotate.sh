ce47914e788f58c5bb088f69dd26e1f45710844a
erikavpond <erikavpond@gmail.com>
2025-11-07 13:04:14 +0000

    Update variant-call_filter_annotate.sh

--- variant-call_filter_annotate.sh
+++ variant-call_filter_annotate.sh
@@ -1,4 +1,4 @@
-## Using original trio of sorted BAM files.
+## Using original trio of sorted BAM files. Ensure all files to start inc ref genome are in same directory
 
 
 ## CALLING
@@ -6,15 +6,13 @@ freebayes -f hg19_ch8.fa Father_bwa.sorted.bam Mother_bwa.sorted.bam Proband_bwa
 # -f denotes file, so the reference file which is Chr8 on hg19, here.
 
 
-
 ## FILTERING
 bcftools filter -i 'QUAL>30 && MIN(FMT/DP)>10' Trio_raw.vcf -o Trio_filtered.vcf
-
 grep -v '^#' trio.freebayes.vcf | awk '($10 ~ /1\/1/ && $11 ~ /0\/1/ && $12 ~ /0\/1/)' > recessive_hits.vcf
 # specific to this case of osteopetrosis autosomal recessive
 
 
-
 ## ANNOTATION
-vep -i Trio_filtered.vcf -o Trio_annotated.txt --cache --symbol
+vep -i Trio_filtered.vcf > Trio_annotated.txt --cache --symbol
 --canonical --vcf
+#
