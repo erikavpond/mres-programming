ed34f7f9cb60e0f37f85c9111cd00660c5935e4e
erikavpond <erikavpond@gmail.com>
2025-11-15 02:54:26 +0000

    Comparative Analysis Using R

--- comparative-analysis.R
+++ comparative-analysis.R
@@ -3,39 +3,39 @@ library("vcfR")
 library("ggplot2")
 library("GenomicRanges")
 library("dplyr")
-library(patchwork)
 
 # setwd("~/Desktop/WES-data")
 
-gatk_vcf <- readVcf("proband_data.vcf", genome = "hg19") 
-filtered_gatkvcf <- subset(gatk_vcf, QUAL>30)
-fb_vcf <- readVcf("freebayes_vcf.vcf", genome = "hg19")
-filtered_fbvcf <- subset(fb_vcf, QUAL>30)
+gatk_vcf <- readVcf("proband_gatk.vcf", genome = "hg19") 
+filtered_gatkvcf <- subset(gatk_vcf, QUAL>30) #filtering
+fb_vcf <- readVcf("proband_fb.vcf", genome = "hg19")
+filtered_fbvcf <- subset(fb_vcf, QUAL>30) #filtering
 gr_gatk <- rowRanges(filtered_gatkvcf)
 gr_fb <- rowRanges(filtered_fbvcf)
 
 
-overlap <- (intersect(names(gr_gatk), names(gr_fb)))
-overlap
+# Finding unique and shared variants 
+
+overlap <- (intersect(names(gr_gatk), names(gr_fb))) #finding shared variants of each
+head(overlap)
 
 unique_gatk <- setdiff(names(gr_gatk), names(gr_fb)) 
-uniquegr_gatk <- gr_gatk[names(gr_gatk) %in% unique_gatk] #
-unique_fb <- setdiff(names(gr_fb), names(gr_gatk)) #lists names of variants unique to freebayes calls
-uniquegr_fb <- gr_fb[names(gr_fb) %in% unique_fb] #
-
+uniquegr_gatk <- gr_gatk[names(gr_gatk) %in% unique_gatk] #lists names of variants unique to gatk-hc calls
+unique_fb <- setdiff(names(gr_fb), names(gr_gatk)) 
+uniquegr_fb <- gr_fb[names(gr_fb) %in% unique_fb] #lists names of variants unique to freebayes calls
 numunique_gatk <- length(uniquegr_gatk)
 numunique_fb <- length(unique_fb)
 
-one_data <- data.frame(
+overlap_df <- data.frame(
   Category= c("GATK only", "FreeBayes only", "Overlap"),
   Count= c(numunique_gatk, numunique_fb, length(overlap))
 )
-(chart1=ggplot(one_data, aes(x="", y=Count, fill=Category)) +
+(chart1=ggplot(overlap_df, aes(x="", y=Count, fill=Category)) +
   geom_col() +
   theme_minimal() +
-  labs(title = "Overlap of variants between GATK and FreeBayes", y= "Number of Variants", x="")
+  labs(title = "Overlap of Variants Discovered", y= "Number of Variants", x="")
 )
-ggsave(chart1, filename= "overlaps.pdf", width = 6, height = 6)
+ggsave(chart1, filename= "overlaps.pdf", width = 6, height = 6) #saves first chart as pdf
 
 
 
@@ -58,10 +58,10 @@ get_variant_type <- function(vcf) {
                   ifelse(ref_len < alt_len, "Insertion",
                   ifelse(ref_len > alt_len, "Deletion", "MNP/Other"))))
   return(variant_type)
-}
+} #creating a function that find the variant type (SNV/indel/etc) for each in list
 
-gatk_types <- get_variant_type(filtered_gatkvcf)
-fb_types <- get_variant_type(filtered_fbvcf)
+gatk_types <- get_variant_type(filtered_gatkvcf) #apply function
+fb_types <- get_variant_type(filtered_fbvcf) #apply function
 gatk_dftype <- data.frame(Caller="GATK", Type= gatk_types)
 fb_dftype <- data.frame(Caller="FreeBayes", Type= fb_types)
 types_df <- bind_rows(gatk_dftype, fb_dftype) %>%
@@ -71,12 +71,12 @@ type_summary <- types_df %>%
   group_by(Caller, Type) %>%
   summarise(Count= n(), .groups = "drop") %>%
   group_by(Caller) %>%
-  mutate(Proportion = Count / sum(Count))
+  mutate(Proportion = Count / sum(Count)) #reformatting data frame for visualisation
 
 cols <- c("SNV" = "#56B4E9",
           "Insertion" = "#009E73",
           "Deletion" = "#E69F00",
-          "MNP/Other" = "#D55E00")
+          "MNP/Other" = "#D55E00") #choosing colours!
 
 (pie_gatk=ggplot(filter(type_summary, Caller== "GATK"),
                  aes(x="", y= Proportion, fill= Type)) +
@@ -88,7 +88,7 @@ cols <- c("SNV" = "#56B4E9",
     theme(legend.position = "right",
           plot.title = element_text(hjust = 0.5))
 )
-ggsave(pie_gatk, filename= "piechart_gatk_variants.pdf")
+ggsave(pie_gatk, filename= "piechart_gatk_variants.pdf") #saves pie chart for gatk variants
 
 (pie_fb=ggplot(filter(type_summary, Caller == "FreeBayes"),
                  aes(x="", y= Proportion, fill= Type)) +
@@ -100,12 +100,13 @@ ggsave(pie_gatk, filename= "piechart_gatk_variants.pdf")
     theme(legend.position = "right",
           plot.title = element_text(hjust = 0.5))
 )
-ggsave(pie_fb, filename= "piechart_fb_variants.pdf")
+ggsave(pie_fb, filename= "piechart_fb_variants.pdf") #saves pie chart for freebayes variants
 
 
 type_summary %>%
   mutate(Percent = round (Proportion *100, 2)) %>%
   select(Caller, Type, Count, Percent)
+type_summary
          
 
 
@@ -119,8 +120,8 @@ get_titv <- function(vcf) {
   alt_alleles <- sapply(alt(vcf), function(x) {
     if (length(x) == 0) return(NA_character_)
     as.character(x[1])
-  })
-  snv_mask <-nchar(ref_alleles) == 1 &nchar(alt_alleles) == 1
+  }) 
+  snv_mask <- nchar(ref_alleles) == 1 &nchar(alt_alleles) == 1
   ref_alleles <- ref_alleles[snv_mask]
   alt_alleles <- alt_alleles[snv_mask]
   transitions <- ((ref_alleles == "A" & alt_alleles == "G") |
@@ -137,14 +138,13 @@ get_titv <- function(vcf) {
     transversions = tv_count,
     TiTv_ratio = ratio
   ))
-}
+} #create function to find transition transversion ratio
 
 gatk_titv <- get_titv(filtered_gatkvcf)
 fb_titv <- get_titv(filtered_fbvcf)
 titv_summary <- bind_rows(
   data.frame(Caller="GATK", gatk_titv),
-  data.frame(Caller="FreeBayes", fb_titv)
-)
+  data.frame(Caller="FreeBayes", fb_titv))
 titv_summary
 
 (titv_plot=ggplot(titv_summary, aes(x=Caller, y=TiTv_ratio, fill=Caller))+
@@ -156,6 +156,4 @@ titv_summary
     theme_minimal()+
     theme(legend.position = "none")
 )
-ggsave(titv_plot, filename="titv_summary.pdf")
-
-)
+ggsave(titv_plot, filename="titv_summary.pdf") #saves TiTv ratio summary as pdf
