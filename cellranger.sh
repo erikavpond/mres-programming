bf7a7f55c0440dce66f15343c4230a0f72dfdae8
erikavpond <erikavpond@gmail.com>
2025-11-25 12:29:53 +0000

    Create cellranger.sh for data processing
    Add initial cellranger.sh script for processing data

---
+++ cellranger.sh
@@ -0,0 +1,12 @@
+#mkref/
+
+%h%-$
+%h%-$ cellranger count \
+      --id
+      --fastqs
+      --transcriptome
+      --
+      --
+#folder of outputs: bams, bais, matrices, summary csv
+#matrix folder: barcodes tsv, features tsv, matrix mtx.
+
